3c68360818d208aedd27f9dd67ebfb16924f8e52
Audumbar Kalokhe <audumbar.mescorp@gmail.com>
2024-05-14 16:32:21 +0530

    ShubhamKalokhe Commit odd or Even Number

--- LetsMakeLogic_1/src/com/C/Test.java
+++ LetsMakeLogic_1/src/com/C/Test.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Test {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter Number");
+	System.out.println("Enter Number here");
 	int number=sc.nextInt();
 	if(number%2==0) {
 		System.out.println("Even Number");
